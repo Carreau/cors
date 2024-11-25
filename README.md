dd34711282269f556239fc6ec3add5f04c013078
M Bussonnier <bussonniermatthias@gmail.com>
2024-11-25 11:29:22 +0100

    strip ununsed code

--- README.md
+++ README.md
@@ -1,20 +1,8 @@
 # cors
 
-Simple CORS proxy
+Simple proxy to add CORS request for pypi.anaconda.org package index.
 
-## Usage
-
-```
-https://cors.ryanking13.workers.dev/?u=https://www.google.com
-```
-
-__WARNING:__
-
-You can use this demo site for testing.
-
-However, DO NOT abuse the demo site for production.
-
-See [Making your own CORS proxy](#making-your-own-cors-proxy).
+Adapted from ryanking13/cors
 
 
 ## Making your own CORS proxy
@@ -22,6 +10,5 @@ See [Making your own CORS proxy](#making-your-own-cors-proxy).
 1. [Learn about Cloudflare Workers](https://workers.cloudflare.com/)
 2. Fork this repository
 3. Change `accound_id` in [wrangler.toml](./wrangler.toml) to your Account ID
-4. (Optional) Set `APP_NAME`, `HOSTNAME`, `APP_KEY` (Ingestion key) for [LogDNA](https://logdna.com/) logging
   - If logging is not needed, set `APP_KEY` to empty string `""`. 
 5. `wrangler publish` ✨
--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const APP_NAME = ''
-const HOSTNAME = ''
-const APP_KEY = ''
-
 async function handleRequest(request) {
   const url = new URL(request.url)
   const pathParts = url.pathname.split('/').filter((part) => part.length > 0)
-  console.log('Path::', url.pathname, pathParts)
   const apiurl = 'https://pypi.anaconda.org'
   // Rewrite request to point to API url. This also makes the request mutable
   // so we can add the correct Origin header to make the API server think
   // that this request isn't cross-site.
-  //
   if (pathParts.length < 2) {
     return new Response(
       'Internal Server Error: Invalid path need at least 2 path parts',
@@ -32,7 +26,7 @@ async function handleRequest(request) {
 }
 
 function handleOptions(request) {
-  // Make sure the necesssary headers are present
+  // Make sure the necessary headers are present
   // for this to be a valid pre-flight request
   if (
     request.headers.get('Origin') !== null &&
@@ -56,60 +50,6 @@ function handleOptions(request) {
   }
 }
 
-// loggin code adapted from: https://github.com/adaptive/cf-logdna-worker
-const getLogData = (request) => {
-  let data = {
-    app: APP_NAME,
-    timestamp: Date.now(),
-    meta: {
-      ua: request.headers.get('user-agent'),
-      referer: request.headers.get('Referer') || 'empty',
-      ip: request.headers.get('CF-Connecting-IP'),
-      countryCode: (request.cf || {}).country,
-      colo: (request.cf || {}).colo,
-      url: request.url,
-      method: request.method,
-      x_forwarded_for: request.headers.get('x_forwarded_for') || '0.0.0.0',
-      asn: (request.cf || {}).asn,
-      cfRay: request.headers.get('cf-ray'),
-      tlsCipher: (request.cf || {}).tlsCipher,
-      tlsVersion: (request.cf || {}).tlsVersion,
-      clientTrustScore: (request.cf || {}).clientTrustScore,
-    },
-  }
-  data.line = `${data.meta.countryCode} ${data.meta.ip} ${data.meta.referer} ==> ${data.meta.url}`
-  return data
-}
-
-const postLog = (request) => {
-  let data = JSON.stringify({ lines: [request] })
-  let myHeaders = new Headers()
-  myHeaders.append('Content-Type', 'application/json; charset=UTF-8')
-  myHeaders.append('Authorization', 'Basic ' + btoa(APP_KEY + ':'))
-  try {
-    return fetch(
-      'https://logs.logdna.com/logs/ingest?tag=worker&hostname=' + HOSTNAME,
-      {
-        method: 'POST',
-        headers: myHeaders,
-        body: data,
-      },
-    ).then((r) => {
-      requests = []
-    })
-  } catch (err) {
-    //console.log(err.stack || err)
-  }
-}
-
-const log = async (event) => {
-  if (APP_KEY === '') {
-    return new Promise()
-  }
-
-  return postLog(getLogData(event.request))
-}
-
 addEventListener('fetch', (event) => {
   const request = event.request
   const url = new URL(request.url)
@@ -121,10 +61,7 @@ addEventListener('fetch', (event) => {
     request.method === 'HEAD' ||
     request.method === 'POST'
   ) {
-    // Handle requests to the API server
-    //const loggingEvent = log(event)
     const response = handleRequest(request)
-    //event.waitUntil(loggingEvent)
     event.respondWith(response)
   } else {
     event.respondWith(async () => {
@@ -141,6 +78,6 @@ addEventListener('fetch', (event) => {
 // all responses to OPTIONS requests.
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 }
